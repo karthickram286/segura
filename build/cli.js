a8e2162a0775030684c5d685b2214a3e18ccaefd
Karthick R <karthick.ra@samsung.com>
2020-07-28 20:00:41 +0530

    cli added

---
+++ build/cli.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const cli = (args) => {
+    console.log('CLI works');
+};
+exports.cli = cli;
---
+++ src/cli.ts
@@ -0,0 +1,5 @@
+const cli = (args: string[]) => {
+  console.log('CLI works');
+};
+
+export { cli };
